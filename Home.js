85f777a28b99a05d0cd288aa93c6b35dfec6dcf6
mithil1593 <mithil7257@gmail.com>
2024-11-16 21:13:56 +0530

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,19 @@
+function Power(appliance) {
+    alert(appliance.charAt(0).toUpperCase() + appliance.slice(1) + " Operated");
+}
+
+function Temperature(value) {
+    alert("AC temperature set to " + value + "°C");
+}
+
+function FanSpeed(value) {
+    alert("Fan speed set to " + value);
+}
+
+function adjustBrightness(value) {
+    alert("Light brightness adjusted to " + value + "%");
+}
+
+function changeColor(value) {
+    alert("Light color changed to " + value);
+}
